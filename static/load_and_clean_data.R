aa9589b18f3f92644f35510217cf0086e9c0b29e
#Jonathan Aguirre <jon306@bu.edu>
2022-12-03 17:05:31 -0500

    made a world dataset with total food loss based on production

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -21,13 +21,10 @@ Food_waste$loss_quantity <- NULL
 Food_waste_clean <- Food_waste %>% group_by(year, country, commodity,m49_code) %>%
   summarise(mean_loss_percentage = mean(loss_percentage))
 
-#this is how we get the countries into regions
-Food_waste_clean <- Food_waste_clean %>%
-  mutate(country_region = countrycode(m49_code, origin = "iso3n", destination = "region"))
+Food_waste_clean <- Food_waste_clean[c(1,5)]
+Food_waste_clean <- Food_waste_clean %>% group_by(year) %>% summarise(count = n(), sum = sum(mean_loss_percentage,na.rm = TRUE)/count) #this is the best version of food clean
 
-#summarise(commodity = sum(commodity))
-
-## CLEAN the Food_production data:
+## CLEAN the Food_production data: ====================================================================Food Production Calculations
 
 Food_production <- read_csv(here::here("dataset-ignore", "Production_All(Normalized).csv"))
 
@@ -43,12 +40,39 @@ Food_production$Flag <- NULL
 Food_production$`Flag Description`<- NULL
 
 Food_production_clean <- filter(Food_production, Food_production$Element == "Production")
-  colnames(Food_production_clean)[2] <- "M49Code"
-  
+colnames(Food_production_clean)[2] <- "M49Code"
+
 
 #MX49 <- gsub('[^[:alnum:] ]', ' ' , Food_production_clean)
 as.numeric( sub("'","",Food_production_clean$M49Code, fixed = TRUE))
 Food_production_clean <- Food_production_clean %>% mutate(M49 = as.numeric( sub("'","",Food_production_clean$M49Code, fixed = TRUE)))  
+
+Food_production_clean1 <- Food_production_clean %>% filter(M49 == 1)
+fasd <- Food_production_clean1 %>% filter(M49 = NA)
+
+Food_production_clean1 <- Food_production_clean1[-c(1:7,9,11)]
+Food_production_clean2 <- Food_production_clean1 %>% group_by(Year) %>% summarise(Production = sum(Value,na.rm = TRUE))  #best food prod version
+
+#=============================================================================
+
+
+#===========================combining production and food loss
+Food_production_clean2 <- Food_production_clean2[-c(1:5,7),]
+Food_waste_clean <- Food_waste_clean[-c(55),]
+
+worldprod <- left_join(Food_production_clean2,Food_waste_clean, by = c("Year" = "year"))
+worldprod <- worldprod[-c(3)]
+worldprod <- worldprod %>% group_by(Year) %>% summarise(Loss = Production * (sum/100), Production, LossPercentage = sum)
+#===========================
+#this is how we get the countries into regions
+Food_waste_clean <- Food_waste_clean %>%
+  mutate(country_region = countrycode(m49_code, origin = "iso3n", destination = "region")) #this separates countries into regions, may not need it for final version
+#=========================
+
+
+
+
+
 
 ## CLEAN the GDP data:
 
