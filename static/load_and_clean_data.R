1c1f47d51d267a3485a16ac80eb9364950c8c1b1
Jason (Tzu Wei) Liu <jtwliu@bu.edu>
2022-12-07 19:45:21 -0500

    load and clean

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -21,8 +21,8 @@ Food_waste$loss_quantity <- NULL
 Food_waste_clean <- Food_waste %>% group_by(year, country, commodity, m49_code) %>%
   summarise(mean_loss_percentage = mean(loss_percentage))
 
-Food_waste_clean <- Food_waste_clean[c(1,5)]
-Food_waste_clean <- Food_waste_clean %>% group_by(year) %>% summarise(count = n(), sum = sum(mean_loss_percentage, na.rm = TRUE)/count) #this is the best version of food clean
+Food_waste_clean1 <- Food_waste_clean[c(1,5)]
+Food_waste_clean1 <- Food_waste_clean1 %>% group_by(year) %>% summarise(count = n(), sum = sum(mean_loss_percentage, na.rm = TRUE)/count) #this is the best version of food clean
 
 ## CLEAN the Food_production data: ==================================================================== Food Production Calculations
 
@@ -44,7 +44,7 @@ colnames(Food_production_clean)[2] <- "M49Code"
 
 
 #MX49 <- gsub('[^[:alnum:] ]', ' ' , Food_production_clean)
-as.numeric( sub("'","",Food_production_clean$M49Code, fixed = TRUE))
+#as.numeric( sub("'","",Food_production_clean$M49Code, fixed = TRUE))
 
 Food_production_clean <- Food_production_clean %>% mutate(M49 = as.numeric( sub("'","",Food_production_clean$M49Code, fixed = TRUE)))  
 
