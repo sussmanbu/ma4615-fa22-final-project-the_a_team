d9c732bf6dfd9d6128f0ace1f9e3fc980ba1c6fa
Italo Duran <duran01@bu.edu>
2022-12-19 18:37:19 -0500

    done_with_interactive

--- shiny_app/try3/app.R
+++ shiny_app/try3/app.R
@@ -37,88 +37,89 @@ ui <- fluidPage(
                                    helpText("Select any of the filters below to observe changes around the world"),
                                    selectInput(inputId = "country", label = "country",choices = c("world",unique(Food_waste$country)),selected = "country"),
                                    selectInput(inputId = "commodity", label = "commodity",choices = c("world",unique(Food_waste$commodity)),selected = "commodity")),
-                      mainPanel(plotOutput(outputId ="dotPlot"),
-                        #plotlyOutput(outputId ="dotPlot"),
-                        dataTableOutput(outputId ="table")
+                      mainPanel(#plotOutput(outputId ="dotPlot"), 
+                                #br(),
+                                plotlyOutput(outputId ="dotPlot"),
+                                br(),
+                                dataTableOutput(outputId ="table")
                         ))) )
 ###################################################################################################
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-    #plot1 <-
-    # ggplot(Food_waste, aes(x = year, y = mean_loss_percentage, color = country)) +
-    # geom_point(alpha = 0.3) + #geom_smooth(size=0.5, se = FALSE) + #xlim(data_yr[1],data_yr[2]) + ylim(0,70) + #se = false, is to remove the confidence bands
-    # ggtitle("Proportion of food waste since 1970") +
-    # xlab("Year") + ylab("Food waste by percentage")  #+ labs(color = "commodity")
+  # This section is to run the graph above but with plotly:
+  # we had to make filters for all possible selectors for the tab window but to also show the world data:
+  output$dotPlot <- renderPlotly({
+    if (input$country == "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(year >= input$year_range[1],
+                                                                                                year <= input$year_range[2])}
+    if (input$country == "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter( commodity == input$commodity,
+                                                                                                 year >= input$year_range[1],
+                                                                                                 year <= input$year_range[2])}
+    if (input$country != "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(country == input$country,
+                                                                                                year >= input$year_range[1],
+                                                                                                year <= input$year_range[2])}
+    if (input$country != "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter(country == input$country,
+                                                                                                commodity == input$commodity,
+                                                                                                year >= input$year_range[1],
+                                                                                                year <= input$year_range[2])}
 
-     #p1 <- plot1 + guides(color = FALSE) + scale_x_continuous(,10)
-    # #plot_ly(Food_waste,  x = ~year, y = ~mean_loss_percentage, color = ~country ) %>% add_markers()
-    # plotly::ggplotly(p1)
-     #p1
-
-  # This one is the normal not plotly graph:
+    plot1 <- ggplot(data_yr,aes(x = year, y = mean_loss_percentage, color = country)) + 
+      geom_point(alpha = 0.3) + geom_smooth(size=0.5, se = FALSE) + ggtitle("Proportion of food waste since 1970") + 
+      xlab("Year") + ylab("Food waste by percentage")
     
-    # plot1 <- ggplot(Food_waste, aes(x = year, y = mean_loss_percentage, color = country)) +
-    #   geom_point(alpha = 0.3)+ geom_smooth(size=0.5, se = FALSE) + ggtitle("Proportion of food waste since 1970") +
-    #   xlab("Year") + ylab("Food waste by percentage") + xlim(data_yr[1] , data_yr[2]) #+ ylim(0,70)
-    # 
-    # p1 <- plot1 + guides(color = FALSE) + scale_x_continuous(, 10)
-    # 
-    # p1
-  #})
-
-  
-  output$dotPlot <- renderPlot({
-    
-    if (input$country == "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(year >= input$year_range[1], 
-                                                                                                  year <= input$year_range[2])}
-    
-    if (input$country == "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter( commodity == input$commodity, 
-                                                                                                  year >= input$year_range[1], 
-                                                                                                  year <= input$year_range[2])}
-    
-    if (input$country != "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(country == input$country, 
-                                                                                                  year >= input$year_range[1], 
-                                                                                                  year <= input$year_range[2])}
-    
-    if (input$country != "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter(country == input$country, 
-                                                                                                  commodity == input$commodity, 
-                                                                                                  year >= input$year_range[1], 
-                                                                                                  year <= input$year_range[2])}
-
-    plot1 <- ggplot(data_yr,aes(x = year, y = mean_loss_percentage, color = country)) + #breaks = year_range
-      geom_point(alpha = 0.3)+ geom_smooth(size=0.5, se = FALSE) + ggtitle("Proportion of food waste since 1970") +
-      xlab("Year") + ylab("Food waste by percentage") #+ xlim(as.data.frame(x[1])  , as.data.frame(x[2]) ) #+ ylim(0,70)
-
     p1 <- plot1 + guides(color = FALSE) + scale_x_continuous(, 10)
-
+    
     p1
     
-  })
-  
-  br()
-  
-  # this one works, but it only shows one country at a time...
+   })
+  #   
+# This section is to run the graph above but w/o plotly:
+  # output$dotPlot <- renderPlot({
+  #   
+  #   if (input$country == "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(year >= input$year_range[1],
+  #                                                                                                 year <= input$year_range[2])}
+  #   
+  #   if (input$country == "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter( commodity == input$commodity,
+  #                                                                                                 year >= input$year_range[1],
+  #                                                                                                 year <= input$year_range[2])}
+  #   
+  #   if (input$country != "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(country == input$country,
+  #                                                                                                 year >= input$year_range[1],
+  #                                                                                                 year <= input$year_range[2])}
+  #   
+  #   if (input$country != "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter(country == input$country,
+  #                                                                                                 commodity == input$commodity,
+  #                                                                                                 year >= input$year_range[1],
+  #                                                                                                 year <= input$year_range[2])}
+  # 
+  #   plot1 <- ggplot(data_yr,aes(x = year, y = mean_loss_percentage, color = country)) +
+  #     geom_point(alpha = 0.3) + geom_smooth(size=0.5, se = FALSE) + ggtitle("Proportion of food waste since 1970") + 
+  #     xlab("Year") + ylab("Food waste by percentage")
+  # 
+  #   p1 <- plot1 + guides(color = FALSE) + scale_x_continuous(, 10)
+  # 
+  #   p1
+  # 
+  # })
+
+# This section is for the interactive table:
+  # we had to make filters for all possible selectors for the tab window but to also show the world data:
    output$table <- renderDataTable({
-    
+     
      if (input$country == "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(year >= input$year_range[1], 
                                                                                                  year <= input$year_range[2])}
-     
      if (input$country == "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter( commodity == input$commodity, 
                                                                                                   year >= input$year_range[1], 
                                                                                                   year <= input$year_range[2])}
-     
      if (input$country != "world" & input$commodity == "world"){data_yr <- Food_waste %>% filter(country == input$country, 
                                                                                                  year >= input$year_range[1], 
                                                                                                  year <= input$year_range[2])}
-     
      if (input$country != "world" & input$commodity != "world"){data_yr <- Food_waste %>% filter(country == input$country, 
                                                                                                  commodity == input$commodity, 
                                                                                                  year >= input$year_range[1], 
                                                                                                  year <= input$year_range[2])}
      data_yr
 
-  })
+  }, options = list(pageLength = 10))
   
     
 }
